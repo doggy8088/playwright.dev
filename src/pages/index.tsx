9d750cd4e9fbcb49b2fd9a01b4ce42c29a8854a2
Will <doggy.huang@gmail.com>
2024-09-08 23:13:18 +0800

    更新首頁文案

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,13 @@ const features = [
     title: "任何瀏覽器 \u2022 任何平台 \u2022 一個 API",
     description: <>
       <p>
-        <b>跨瀏覽器：</b> Playwright 支援所有現代渲染引擎，包括 Chromium、WebKit 和 Firefox。
+        <b>跨瀏覽器 \u2022 </b> Playwright 支援所有現代渲染引擎，包括 Chromium、WebKit 和 Firefox。
       </p>
       <p>
-        <b>跨平台：</b> 在 Windows、Linux 和 macOS 上進行測試，本地或在 CI 上，無頭或有頭。
+        <b>跨平台 \u2022 </b> 在 Windows、Linux 和 macOS 上進行測試，本地或在 CI 上，無頭或有頭。
       </p>
       <p>
-        <b>跨語言：</b> 使用 Playwright API 在 <a
+        <b>跨語言 \u2022 </b> 使用 Playwright API 在 <a
         href="https://playwright.dev/docs/intro">TypeScript</a>、<a
         href="https://playwright.dev/docs/intro">JavaScript</a>、<a
         href="https://playwright.dev/python/docs/intro">Python</a>、<a
@@ -26,7 +26,7 @@ const features = [
         href="https://playwright.dev/java/docs/intro">Java</a> 中。
       </p>
       <p>
-        <b>測試行動網頁：</b> Google Chrome for Android 和 Mobile Safari 的原生行動模擬。相同的渲染引擎在您的桌面和雲端中運行。
+        <b>測試行動網頁 \u2022 </b> Google Chrome for Android 和 Mobile Safari 的原生行動模擬。相同的渲染引擎在您的桌面和雲端中運行。
       </p>
     </>,
   },
@@ -39,13 +39,13 @@ const features = [
     description: (
       <>
         <p>
-          <b>自動等待。</b> Playwright 在執行操作之前等待元素可操作。它還有一套豐富的內省事件。兩者的結合消除了人工超時的需要 - 這是導致不穩定測試的主要原因。
+          <b>自動等待 \u2022 </b> Playwright 在執行操作之前等待元素可操作。它還有一套豐富的內省事件。兩者的結合消除了人工超時的需要 - 這是導致不穩定測試的主要原因。
         </p>
         <p>
-          <b>以網頁為主的斷言。</b> Playwright 斷言專為動態網頁設計。檢查會自動重試，直到滿足必要條件。
+          <b>以網頁為主的斷言 \u2022 </b> Playwright 斷言專為動態網頁設計。檢查會自動重試，直到滿足必要條件。
         </p>
         <p>
-          <b>追蹤。</b> 配置測試重試策略，捕獲執行追蹤、影片、截圖以消除不穩定性。
+          <b>追蹤 \u2022 </b> 配置測試重試策略，捕獲執行追蹤、影片、截圖以消除不穩定性。
         </p>
       </>
     ),
@@ -58,13 +58,13 @@ const features = [
           瀏覽器在不同程序中運行屬於不同來源的網頁內容。Playwright 與現代瀏覽器架構保持一致，並在程序外運行測試。這使得 Playwright 擺脫了典型的程序內測試執行器限制。
         </p>
         <p>
-          <b>多重一切。</b> 測試場景涵蓋多個 <b>頁籤(tabs)</b>、多個 <b>來源(origins)</b> 和多個 <b>用戶(users)</b>。為不同用戶建立不同上下文的場景，並在一個測試中對您的伺服器進行測試。
+          <b>多重一切 \u2022 </b> 測試場景涵蓋多個 <b>頁籤(tabs)</b>、多個 <b>來源(origins)</b> 和多個 <b>用戶(users)</b>。為不同用戶建立不同上下文的場景，並在一個測試中對您的伺服器進行測試。
         </p>
         <p>
-          <b>可信事件。</b> 懸停元素，與動態控件交互，生成可信事件。Playwright 使用真實的瀏覽器輸入管道，與真實用戶無法區分。
+          <b>可信事件 \u2022 </b> 懸停元素，與動態控件交互，生成可信事件。Playwright 使用真實的瀏覽器輸入管道，與真實用戶無法區分。
         </p>
         <p>
-          <b>測試 Frames，穿透 Shadow DOM。</b> Playwright 選擇器穿透 Shadow DOM，並允許無縫進入 Frames。
+          <b>測試 Frames，穿透 Shadow DOM \u2022 </b> Playwright 選擇器穿透 Shadow DOM，並允許無縫進入 Frames。
         </p>
       </>
     ),
@@ -78,10 +78,10 @@ const features = [
     description: (
       <>
         <p>
-          <b>瀏覽器上下文：</b> Playwright 為每個測試建立一個瀏覽器上下文。瀏覽器上下文相當於一個全新的瀏覽器配置文件。這提供了完全的測試隔離，且沒有任何開銷。建立一個新的瀏覽器上下文只需幾毫秒。
+          <b>瀏覽器上下文 \u2022 </b> Playwright 為每個測試建立一個瀏覽器上下文。瀏覽器上下文相當於一個全新的瀏覽器配置文件。這提供了完全的測試隔離，且沒有任何開銷。建立一個新的瀏覽器上下文只需幾毫秒。
         </p>
         <p>
-          <b>僅登入一次：</b> 保存上下文的身份驗證狀態，並在所有測試中重用它。這繞過了每個測試中的重複登錄操作，但仍提供了獨立測試的完全隔離。
+          <b>僅登入一次 \u2022 </b> 保存上下文的身份驗證狀態，並在所有測試中重用它。這繞過了每個測試中的重複登錄操作，但仍提供了獨立測試的完全隔離。
         </p>
       </>
     ),
@@ -206,7 +206,7 @@ const Home: React.FC = () => {
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className={clsx("hero__title", styles.heroTitle)}>
-            <span className={styles.highlight}>Playwright</span> 使現代網頁應用的 end-to-end 測試變得可靠。
+            <span className={styles.highlight}>Playwright</span> 為現代網頁應用提供穩定可靠的<br/>端到端測試體驗。
           </h1>
           <div className={styles.buttons}>
             <Link
